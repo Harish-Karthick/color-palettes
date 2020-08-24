198477e3c8cf4e43e0d122e6737f166002078b47
Harish Karthik <hhkk0010@outlook.com>
2020-08-24 16:20:07 +0530

    Initial commit - created basic colors file and palette component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import seedColors from "./utils/seedColors";
+import PaletteView from "./components/PaletteView";
+function App() {
+  return (
+    <div className='App'>
+      <PaletteView color={seedColors[0]} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PaletteView.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PaletteView extends Component {
+  render() {
+    return <h1>Palette </h1>;
+  }
+}
+
+export default PaletteView;

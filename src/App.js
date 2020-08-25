3c1b9ca4c5d02ba3ec28c33f7096e56ad2970eb3
Harish Karthik <hhkk0010@outlook.com>
2020-08-25 12:38:30 +0530

    Added chroma js and a color generator helper function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import seedColors from "./utils/seedColors";
+import { generateColors } from "./utils/colorHelpers";
 import PaletteView from "./components/PaletteView";
 function App() {
+  console.log(generateColors(seedColors[0]));
   return (
     <div className='App'>
       <PaletteView color={{ ...seedColors[0] }} />
